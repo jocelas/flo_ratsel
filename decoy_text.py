aaec06a1cbdbd336fcb40faa9703699cc155c412
jocelas <jonas@bojanovsky.at>
2025-12-31 11:47:44 +0100

    before publish

--- decoy_text.py
+++ decoy_text.py
@@ -7,11 +7,23 @@ total_alphabet = []
 for i in range(len(alphabet)):
     total_alphabet.append(Alphabet[i])
     total_alphabet.append(alphabet[i])
-print(total_alphabet)    
 
 decoy = 'asdfgb cmtz srux vjkjokeay rec DQwEPXY EGUWCNVF znyix'
 deocy_key = list('asdfgbcmtzsruxvjkjokeayrec')
 lower_decoy = decoy.lower()
+
+first_text = "Guten Tag, das hier ist ein decoy text. Er hat keinen weiteren Sinn"
+total_text = first_text + decoy
+
+
+def count_letter(text):
+    count = 0
+    list_text = list(text)
+    for char in list_text:
+        if char in total_alphabet:
+            count = count + 1
+    return count
+
 
 def shift_decoy(n, decoy_in, key_list, alphabet_list):
     decoy_out = list(decoy_in)
@@ -21,3 +33,5 @@ def shift_decoy(n, decoy_in, key_list, alphabet_list):
     return ''.join(decoy_out)
 
 if __name__ == '__main__':
+    count = count_letter(total_text)
+    print(f"Number of letters in text: {count}")
